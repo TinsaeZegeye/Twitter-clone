ca42986b1dcd11f3f696b40e78d129f49364d408
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-09-20 12:29:58 +0300

    Add post section of the Feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,28 @@
 import { SparklesIcon } from '@heroicons/react/outline'
-import React from 'react'
 import Input from './Input'
+import Post from './Post'
 
 export default function Feed() {
+    const posts = [
+        {
+            id: '1', 
+            name: 'Tinsae Zegeye', 
+            username: 'YenYoki', 
+            userImg: '/userpro.jpeg', 
+            postImg: 'https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmF0dXJlfGVufDB8fDB8fHww', 
+            text: 'Nice View!', 
+            timestamp: '2 hours ago', 
+        }, 
+        {
+            id: '2', 
+            name: 'Estifanos Zegeye', 
+            username: 'EstifZ', 
+            userImg: '/userpro2.jpeg', 
+            postImg: 'https://images.unsplash.com/photo-1513836279014-a89f7a76ae86?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D', 
+            text: 'Amazing View!!!', 
+            timestamp: '2 days ago', 
+        }
+    ]
   return (
     <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] max-w-xl flex-grow'>
           <div className='flex item-center py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
@@ -11,7 +31,10 @@ export default function Feed() {
                   <SparklesIcon className='h-5'/>
               </div>
           </div>
-          <Input/>
+      <Input />
+          {posts.map((post) => (
+              <Post key={post.id} post={post} />
+      ))}
     </div>
   )
 }
---
+++ components/Post.jsx
@@ -0,0 +1,43 @@
+import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from '@heroicons/react/outline'
+
+export default function Post({post}) {
+  return (
+    <div className='flex p-3 cursor-pointer border-b border-gray-200'>
+          {/* Main div for post section */}
+
+          <img className='h-11 w-11 rounded-full mr-4' src={post.userImg} alt="User-Profile-Image" />
+          
+          <div >
+              {/* Right section */}
+
+              <div className='flex items-center justify-between'>
+                  {/* Post header part */}
+
+                    <div className='flex space-x-2 items-center whitespace-nowrap'>
+                        {/* User Info part */}
+                        <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.name}</h4>
+                        <span className='text-sm sm:text-[15px]'>@{post.username} -</span>
+                        <span className='text-sm sm:text-[15px] hover:underline'>{ post.timestamp}</span>
+                    </div>
+                        <DotsHorizontalIcon className='h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2' />
+            </div>      
+              
+              {/* Post Text */}
+              <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post.text}</p>
+              
+              {/* Post image */}
+              <img className='rounded-2xl mr-2' src={post.postImg} alt="Post image" />
+
+              <div className='flex justify-between text-gray-500 p-2'>
+                  {/* Post Reaction icons */}
+                  <ChatIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'/>
+                  <TrashIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100'/>
+                  <HeartIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100'/>
+                  <ShareIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'/>
+                  <ChartBarIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'/>
+                  
+              </div>
+          </div>
+    </div>
+  )
+}
