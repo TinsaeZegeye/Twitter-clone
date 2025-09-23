0e57d343821f674beb91708aaa5e2b5256ef30a8
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-09-23 13:58:03 +0300

    Add animation effect using framer motion

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,6 +6,7 @@ import Post from './Post'
 import { useEffect, useState } from 'react'
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 import { db } from '../lib/firebase'
+import {motion, AnimatePresence} from 'motion/react'
 
 export default function Feed() {
     const [posts, setPosts] = useState([]);
@@ -23,9 +24,19 @@ export default function Feed() {
               </div>
           </div>
       <Input />
-          {posts.map((post) => (
-              <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }
--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -3,6 +3,7 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import React, { useState } from 'react'
 import News from './News'
+import {motion, AnimatePresence} from 'motion/react'
 
 export default function Widgets({articles, results}) {
     const [articleNum, setArticleNum] = useState(3);
@@ -19,34 +20,53 @@ export default function Widgets({articles, results}) {
           </div>
 
           <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
-              <h4 className='font-bold text-xl px-4'>What's Happening?</h4>
-              {articles?.slice(0, articleNum).map((article)=>(
-                  <News key={article.title} article={article} />
-              ))}
+        <h4 className='font-bold text-xl px-4'>What's Happening?</h4>
+        <AnimatePresence>
+          {articles?.slice(0, articleNum).map((article, index) => (
+            <motion.div
+              key={`${article.title}-${index}`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <News article={article} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
         
               <button onClick={() => setArticleNum(articleNum + 3)} className={`${articleNum > articles.length - 3 && 'hidden'} text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Show more</button>
         
                 <button onClick={() => setArticleNum(articleNum - 3)} className={`${articleNum <= 3 && 'hidden'} text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Hide</button>
           </div>
           <div className='text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%] sticky top-16'>
-              <h4 className='font-bold text-xl px-4'>Who to Follow?</h4>
-              {results.slice(0, randomUserNum).map((result) => (
-                <div key={result.login.username} className='flex items-center px-4 py-2 cursor-pointer'>
-                      <img className='rounded-full' src={result.picture.thumbnail} alt="User-Profile-Image" width='40'/>
-                      
-                      <div className='truncate leading-5 ml-4'>
-                          <h4 className='font-bold hover:underline text-[14px] truncate'>{result.login.username}</h4>
-                          <h5 className='text-[13px] text-gray-500 truncate'>{result.name.first + ' ' + result.name.last}</h5>
-                      </div>
+        <h4 className='font-bold text-xl px-4'>Who to Follow?</h4>
+        <AnimatePresence>
+          {results.slice(0, randomUserNum).map((result, index) => (
+              <motion.div
+                key={`${result.login.username}-${index}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+              >
+                    <div className='flex items-center px-4 py-2 cursor-pointer hoverEffect'>
+                          <img className='rounded-full' src={result.picture.thumbnail} alt="User-Profile-Image" width='40'/>
+                          
+                          <div className='truncate leading-5 ml-4'>
+                              <h4 className='font-bold hover:underline text-[14px] truncate'>{result.login.username}</h4>
+                              <h5 className='text-[13px] text-gray-500 truncate'>{result.name.first + ' ' + result.name.last}</h5>
+                          </div>
 
-                      <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold cursor-pointer hover:brightness-90'>Follow</button>
-                </div>
-              ))}
-              
-              <button onClick={()=>setRandomUserNum(randomUserNum + 3)} className={`${randomUserNum > results.length - 3 && 'hidden' } text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Show more</button>
-              
-              <button onClick={()=> setRandomUserNum(randomUserNum - 3)} className={`${randomUserNum <= 3 && 'hidden'} text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Hide</button>
-          </div>
+                          <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold cursor-pointer hover:brightness-90'>Follow</button>
+                    </div>
+                  </motion.div>
+                ))}
+        </AnimatePresence>
+            <button onClick={()=>setRandomUserNum(randomUserNum + 3)} className={`${randomUserNum > results.length - 3 && 'hidden' } text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Show more</button>
+            
+            <button onClick={()=> setRandomUserNum(randomUserNum - 3)} className={`${randomUserNum <= 3 && 'hidden'} text-blue-300 pl-4 pb-3 hover:text-blue-400 cursor-pointer`}>Hide</button>
+        </div>
     </div>
   )
 }
