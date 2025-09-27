302e6dbf3aa47d73a904df4fda686da33795c23c
Tinsae Zegeye <tinsae.zegeye@aastustudent.edu.et>
2025-09-27 13:07:32 +0300

    Fix Small Error

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -22,7 +22,7 @@ export default function Feed() {
     }, []);
 
     return (
-        <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] max-w-xl flex-grow'>
+        <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] max-w-xl flex-grow pl-1'>
             <div className='flex item-center py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
                 <h2 className='font-bold text-lg sm:text-xl cursor-pointer flex-grow'>Home</h2>
                 <div className='hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9'>
